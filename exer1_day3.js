051b0a344f7186b83d11f60fd7087f7b3c11b3da
OKwumi <oyekanmikhadijat@gmail.com>
2022-07-04 02:27:31 +0200

    commit 3

---
+++ exer1_day3.js
@@ -0,0 +1,20 @@
+//Exercise 1 of Day3
+/*1.Declare firstName, lastName, country, city, age,
+isMarried, year variable and assign value to it
+and use the typeof operator to check different data types.
+*/
+let firstName='khodeejah'
+let lastName='Oyekanmi'
+let country='Nigeria'
+let city='Osogbo'
+let age=20
+let isNarried=false
+let year=2020
+
+console.log(typeof (firstName))
+console.log(typeof(lastName))
+console.log(typeof(country))
+console.log(typeof(city))
+console.log(typeof(age))
+console.log(typeof(isNarried))
+console.log(typeof(year))
